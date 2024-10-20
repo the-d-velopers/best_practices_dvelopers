f6b09f0dbabab1bbaa97d5a504dc124713be1bae
Carlitos Vargas <carlos.vr56@gmail.com>
2024-10-20 13:51:04 -0300

    feat: set fontFamily

---
+++ lib/app_config/app_theme/app_palette.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppPalette {
+  Color primaryColor = const Color(0xFFc0b898);
+  Color primaryColorDark = const Color(0xFF90886b);
+  Color secondaryColor = const Color(0xFFcd3200);
+  Color backgroundColor = const Color(0xFF1a1a13);
+}
--- lib/app_config/app_theme/app_theme.dart
+++ lib/app_config/app_theme/app_theme.dart
@@ -1,8 +1,10 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 
 class AppTheme {
   static ThemeData lightTheme() {
     return ThemeData(
+      fontFamily: GoogleFonts.lexendDeca().fontFamily,
       colorScheme: ColorScheme.fromSeed(seedColor: Colors.green),
       useMaterial3: true,
     );
